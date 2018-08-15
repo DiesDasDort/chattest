20babf33581a6c5cfec66484b8509622d52c4123
w00t <khang9101993@hotmail.de>
2018-08-15 23:08:28 +0200

    dsa
    dsadsa

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 8080
+var app = express();
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
